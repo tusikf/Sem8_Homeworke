9e812055b30a3e6e78d083209f260172beef4f3f
Nati <tusikf@yandex.ru>
2023-07-19 14:55:09 +0300

    test 4

---
+++ E4/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+Console.Write("Введите размер трехмерной матрицы: ");
+int row = int.Parse(Console.ReadLine()!);
+
+int colom = row;
+int k = row;
+
+int[,,] massive = new int[row,colom,k];
+FillArray(massive);
+
+Console.WriteLine("Получилась матрица: ");
+
+PrintArray(massive);
+Console.WriteLine();
+
+
+
+
+
+void PrintArray(int[,,] mass)
+{
+    for (int i=0; i < mass.GetLength(0); i++)
+    {
+        for (int j=0; j < mass.GetLength(0); j++)
+        {
+            for (int v=0; v < mass.GetLength(0); v++)
+            {
+                Console.Write($"{mass[ i , j , v] }, ({i}, {j}, {v}) ");
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArray(int[,,] mass)
+{
+    for (int i=0; i < mass.GetLength(0); i++)
+    {
+        for (int j=0; j < mass.GetLength(0); j++)
+        {
+            int v = 0;
+            while( v < mass.GetLength(0))
+            {
+                int temp = new Random().Next(0,1000);
+                if (FindeEl(temp,mass[i,j,v])) continue;
+                mass[i,j,v] = temp;
+                v++;
+            }
+        }
+    }
+}
+
+bool FindeEl(int el,int[,,]array)
+{
+    for (int i=0; i < array.GetLength(0); i++)
+    {
+        for (int j=0; j < array.GetLength(0); j++)
+        {
+            for (int v=0; v < array.GetLength(0); v++)
+            {
+                if (array[i,j,v] == el) return true;
+            }
+            
+        }
+    }
+    return false;
+}
---
+++ E5/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int colom = int.Parse(Console.ReadLine()!);
+
+int[,] massive = new int[row,colom];
+
+FillArray(massive);
+
+Console.WriteLine("Получился массив: ");
+PrintArray(massive);
+Console.WriteLine();
+
+
+
+
+
+void PrintArray(int[,] mass)
+{
+    for (int i=0; i < mass.GetLength(0); i++)
+    {
+        for (int j=0; j < mass.GetLength(1); j++)
+        {
+            Console.Write($"{mass[ i , j ] }  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArray(int[,] mass)
+{
+    int k=0;
+    int a = 0;
+    for (int i=0; i < mass.GetLength(0)/2; i++)
+    {
+        for (int j=0; j < mass.GetLength(1)-a; j++)
+        {
+            mass[i,j] = k;
+            k++;
+        }
+        int jj = mass.GetLength(1)-a-1;
+        for (int ii =0; ii < mass.GetLength(0)-a; ii++)
+        {
+            mass[i,jj] = k;
+            k++;
+        }
+        for (int j = mass.GetLength(1)-a; j< 0; j--)
+        {
+            mass[i,j] = k;
+            k++;
+        }
+        int jjj = 0;
+        for (int ii = mass.GetLength(0)-a; ii < 0; i--)
+        {
+            mass[ii,jjj] = k;
+            k++;
+        }
+        a++;
+
+    }
+}
